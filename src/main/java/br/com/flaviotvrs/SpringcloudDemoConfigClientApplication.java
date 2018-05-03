f4a6beab9013d419ae7e35cee4024e71975beb81
Flávio Tavares <flaviotvrs@gmail.com>
2018-05-03 14:23:57 -0300

    initial commit

---
+++ src/main/java/br/com/flaviotvrs/SpringcloudDemoConfigClientApplication.java
@@ -0,0 +1,12 @@
+package br.com.flaviotvrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudDemoConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudDemoConfigClientApplication.class, args);
+	}
+}
---
+++ src/main/java/br/com/flaviotvrs/controller/LuckyWordController.java
@@ -0,0 +1,17 @@
+package br.com.flaviotvrs.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LuckyWordController {
+
+	@Value("${lucky-word}")
+	private String luckyWord;
+	
+	@GetMapping("/lucky-word")
+	public String showLuckyWord() {
+		return "The lucku word is: " + luckyWord;
+	}
+}
